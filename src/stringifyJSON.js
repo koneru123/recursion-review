fa753c89d9f7904d2c107c3bb6e4783cf1706f42
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-03-23 17:21:38 -0700

    stringifyJSON implementation

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,77 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // strings,numbers, arrays, obj, null, undefined,functions
+
+  var resultStr = "";
+
+  if ( typeof obj === "string" ) {
+    return `"${obj}"`;
+  }
+  if ( obj === null ) {
+    return 'null' ;
+  }
+  if ( typeof obj === 'function') {
+    return null;
+  }
+  if( typeof obj === 'number'|| typeof obj === 'boolean' ) {
+    return resultStr + obj ;
+  }
+  if( Array.isArray(obj) ) {
+    var newArr = [];
+    for(var i = 0; i < obj.length; i++) {
+      newArr.push(stringifyJSON(obj[i]));
+    }
+    return "[" + newArr + "]";
+  }
+  if (typeof obj === 'object') {
+    let emptyObjArr = [];
+    for (let [key, value] of Object.entries(obj)) {
+      if (value !== undefined && typeof value !== 'function') {
+        emptyObjArr.push(`${stringifyJSON(key)}:${stringifyJSON(value)}`);
+      }
+    }
+    return '{' + emptyObjArr + '}';
+  }
+   /*  var keys = Object.keys(obj);
+    var count = keys.length;
+
+    for( var key in obj ) {
+      debugger;
+      if( typeof obj[key] === undefined || typeof obj[key] === 'function') {
+       count -= 1
+      } else if (count > 1 ){
+        resultStr += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+        count -= 1
+      } else { resultStr += stringifyJSON(key) + ':' + stringifyJSON(obj[key])}
+    }
+    return '{' + resultStr + '}'; */
+
 };
+
+ /* for (var [key, value] of Object.entries(obj) ) {
+        resultStr += stringifyJSON(key) ':' stringifyJSON(value) + ',';
+    } */
+
+/* else if( Array.isArray(obj) ) {
+  if ( obj.length === 0 ) {
+    return '[]';
+  } else {
+    //var newArr = [];
+    for ( var i = 0; i < obj.length; i++) {
+      resultStr +=  stringifyJSON(obj[i]);
+      //newArr.push(stringifyJSON(obj[i]));
+    }
+  }
+  return '[' + resultStr + ']' ;
+} else if ( typeof obj === "object" ) {
+  if(!obj.hasOwnProperty[key]) {
+    return '{}';
+  }
+
+  for (var [key, Value] of Object.entries(obj) ) {
+      resultStr += stringifyJSON(key) ':' stringifyJSON(value) + ',';
+  }
+  return '{' + resultStr + '}';
+} */
+
